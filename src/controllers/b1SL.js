ad12e2fc8fdd862cd32108c581c583cad0cdf080
Ali207614 <madaminovmuhammadali2003@gmail.com>
2025-10-29 12:51:55 +0500

    fix bug

--- src/controllers/b1SL.js
+++ src/controllers/b1SL.js
@@ -113,7 +113,46 @@ class b1SL {
                 }
             });
     }
+    createBusinessPartner = async ({ Phone1, Phone2, CardName }) => {
 
+        let body = {CardName}
+
+        if (Phone1) {
+            body = {...body, Phone1 }
+        }
+
+        if (Phone2) {
+            body = { ...body, Phone2 }
+        }
+
+        const axios = Axios.create({
+            baseURL: `${this.api}`,
+            timeout: 30000,
+            headers: {
+                'Cookie': get(getSession(), 'Cookie[0]', '') + get(getSession(), 'Cookie[1]', ''),
+                'SessionId': get(getSession(), 'SessionId', '')
+            },
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false,
+            }),
+        });
+        return axios
+            .post(`/BusinessPartners`, body)
+            .then(async ({ data }) => {
+                return data
+            })
+            .catch(async (err) => {
+                if (get(err, 'response.status') == 401) {
+                    let token = await this.auth()
+                    if (token.status) {
+                        return await this.createBusinessPartner({  Phone1, Phone2, CardName })
+                    }
+                    return { status: false, message: token.message }
+                } else {
+                    return { status: false, message: get(err, 'response.data.error.message.value') }
+                }
+            });
+    }
 }
 
 module.exports = new b1SL();
