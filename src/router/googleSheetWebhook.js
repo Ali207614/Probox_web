eea201127f3dc5916a6adc5bc2c5601d7e9cea38
Ali207614 <madaminovmuhammadali2003@gmail.com>
2025-12-04 11:17:38 +0500

    fix bug

--- src/router/googleSheetWebhook.js
+++ src/router/googleSheetWebhook.js
@@ -148,7 +148,7 @@ router.post('/webhook', basicAuth, async (req, res) => {
         ]);
 
         const nValue = lastLead?.nNumeric || 0;
-        const nextStart = nValue > 50 ? nValue - 50 : 2;
+        const nextStart = nValue > 500 ? nValue - 500 : 2;
         const nextEnd = nextStart + 100;
         console.log(`🔍 Checking new rows from ${nextStart} to ${nextEnd}`);
 
