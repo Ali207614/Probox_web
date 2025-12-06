f9246bfc9b30dc7f1245063a85fee51eab775107
Ali207614 <madaminovmuhammadali2003@gmail.com>
2025-12-06 16:03:07 +0500

    fix bug

--- src/repositories/dataRepositories.js
+++ src/repositories/dataRepositories.js
@@ -772,7 +772,7 @@ ORDER BY
                 FROM ${this.db}.INV6 T0
                 JOIN ${this.db}.OINV T1 ON T0."DocEntry" = T1."DocEntry"
                 WHERE T0."DueDate" BETWEEN '${startDate}' AND '${endDate}'
-                  AND T1."Canceled" = 'N'
+                  AND T1."CANCELED" = 'N'
            ) AS "InsTotal",
             SUM(T0."PaidToDate") as "PaidToDate"
         FROM 
