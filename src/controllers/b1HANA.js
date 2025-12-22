a8a817d89ca967cd9a1690e123e08dfd7b689ba4
Ali207614 <madaminovmuhammadali2003@gmail.com>
2025-12-22 12:08:34 +0500

    fix bug

--- src/controllers/b1HANA.js
+++ src/controllers/b1HANA.js
@@ -695,7 +695,7 @@ class b1HANA {
             const total = await LeadModel.countDocuments(filter);
             const rawData = await LeadModel.find(filter)
                 .select(
-                    '_id callCount callCount2 acceptedReason meetingHappened cardCode invoiceCreated invoiceDocEntry invoiceDocNum invoiceCreatedAt isBlocked status jshshir idX branch2 seller n scoring clientName clientPhone source time operator operator2 branch comment meetingConfirmed meetingDate createdAt purchase called answered interested called2 answered2 passportId jshshir2 score mib aliment officialSalary finalLimit finalPercentage'
+                    '_id paymentScore totalContracts openContracts totalAmount totalPaid overdueDebt maxDelay avgPaymentDela callCount callCount2 acceptedReason meetingHappened cardCode invoiceCreated invoiceDocEntry invoiceDocNum invoiceCreatedAt isBlocked status jshshir idX branch2 seller n scoring clientName clientPhone source time operator operator2 branch comment meetingConfirmed meetingDate createdAt purchase called answered interested called2 answered2 passportId jshshir2 score mib aliment officialSalary finalLimit finalPercentage'
                 )
                 .sort({ time: -1 })
                 .skip(skip)
@@ -706,6 +706,13 @@ class b1HANA {
                 n: item.n,
                 id: item._id,
                 paymentScore: item.paymentScore || null,
+                totalContracts: item.totalContracts || null,
+                openContracts: item.openContracts || null,
+                totalAmount: item.totalAmount || null,
+                totalPaid: item.totalPaid || null,
+                overdueDebt: item.overdueDebt || null,
+                maxDelay: item.maxDelay || null,
+                avgPaymentDela: item.avgPaymentDela || null,
                 callCount: item?.callCount || null,
                 callCount2: item?.callCount2 || null,
                 meetingHappened: item.meetingHappened || null,
