f2e375dc1242b5498a22ca61b7f1d28b124ab280
Ali207614 <madaminovmuhammadali2003@gmail.com>
2026-01-05 16:44:22 +0500

    fix bug

--- src/repositories/dataRepositories.js
+++ src/repositories/dataRepositories.js
@@ -1011,7 +1011,6 @@ ORDER BY
         WHERE 
             T0."DueDate" BETWEEN '${startDate}' AND '${endDate}'
             AND T1."CANCELED" = 'N'
-            ${statusCondition}
         GROUP BY 
             T0."DocEntry",
             T0."InstlmntID"
