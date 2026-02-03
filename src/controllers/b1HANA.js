7b4f8076ec56f5d68c316a3d5c0b3ab493de33ad
Ali207614 <madaminovmuhammadali2003@gmail.com>
2026-02-03 17:47:41 +0500

    fix bug

--- src/controllers/b1HANA.js
+++ src/controllers/b1HANA.js
@@ -1017,6 +1017,7 @@ class b1HANA {
                 n: item.n,
                 id: item._id,
                 seen:item?.seen,
+                rejectionReason: item.rejectionReason || null,
                 clientPhone2: item.clientPhone2 || null,
                 address2: item.address2 || null,
                 paymentScore: item.paymentScore || null,
