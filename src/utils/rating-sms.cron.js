a88d96f03694698c1638ba43349be81bbd683c84
Ali207614 <madaminovmuhammadali2003@gmail.com>
2026-03-12 17:06:56 +0500

    fix bug

--- src/utils/rating-sms.cron.js
+++ src/utils/rating-sms.cron.js
@@ -164,8 +164,8 @@ function startRatingSmsCron() {
     // =========================================================================
     // 🛠 TEST UCHUN CRON: Har 1 minutda ishlaydi (Hozir yoniq)
     // =========================================================================
-    //const cronExpression = '* * * * *';
-    const cronExpression = '*/5 * * * *';
+    const cronExpression = '* * * * *';
+    //const cronExpression = '*/5 * * * *';
 
     // =========================================================================
     // 🚀 PROD UCHUN CRON: Har soat boshida ishlaydi (Test tugagach shuni ochasiz)
