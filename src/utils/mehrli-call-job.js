424b7f05719dbe68b29d7ab3b6661e4fbf44fe37
Ali207614 <madaminovmuhammadali2003@gmail.com>
2026-03-17 16:22:28 +0500

    fix bug

--- src/utils/mehrli-call-job.js
+++ src/utils/mehrli-call-job.js
@@ -14,7 +14,7 @@ const TARGET_OPERATOR_CODE = 58;
  */
 function startMehrliCallJob() {
     cron.schedule(
-        '20 16 * * *',
+        '0 10 * * *',
         async () => {
             try {
                 console.log('[CRON] Mehrli Qongiroq job started');
