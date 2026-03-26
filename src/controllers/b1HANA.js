b3a4c122d42f426e29a5eab0e7e03a0ba4d41ebf
Ali207614 <madaminovmuhammadali2003@gmail.com>
2026-03-26 16:11:04 +0500

    fix bug

--- src/controllers/b1HANA.js
+++ src/controllers/b1HANA.js
@@ -192,6 +192,7 @@ class b1HANA {
     onlinePbxWebhook = async (req, res, next) => {
         try {
             const payload = this.normalizeOnlinePbxPayload(req.body);
+            console.log('onlinePbxWebhook', payload);
             const io = req.app.get('io'); // ✅ haqiqiy io
             const result = await handleOnlinePbxPayload(payload, io);
             return res.status(200).json(result);
